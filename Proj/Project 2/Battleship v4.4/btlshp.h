5bf3259818c1e13a513bc96adf186f3cd8dd0ad8
Siffy-Code <101999481+Siffy-Code@users.noreply.github.com>
2022-12-03 17:52:27 -0800

    Create btlshp.h

---
+++ Proj/Project 2/Battleship v4.4/btlshp.h
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/file.h to edit this template
+ */
+
+/* 
+ * File:   BattleshipStructs.h
+ * Author: ptron
+ *
+ * Created on October 29, 2022, 7:42 PM
+ */
+#ifndef BTLSHP_H
+#define BTLSHP_H
+#include "shipDat.h"
+#include "siffy.h"
+#include "plyrSv.h"
+#include "strcts.h"
+
+using namespace std;
+class btlshp{  //basic game functions
+private:
+      
+    plyrSv gamDat;
+    vector<record> Scrs;        //Score storage and display medium in-game.  Placed in text file otherwise
+    //siffy to be the multiple inheritance?
+    fstream file;
+    int maxSpc;
+    string Win;
+public:
+    
+    void chkTrns();
+    void runGam(){gamDat.plyGam(Win,file);}
+    //initialize win as "null"
+    //player selection at each turn function
+    void SUtxt1 (string);                                       //Setup Text for Player Instructions
+    void MenuOpt ();                                            //Displays menu options
+    void swtch(char,bool &);         //The Welcome Screen Menu (score operations and passthrough)
+    btlshp();
+    void setShps();
+        //Initialization Functions - should be grouped into a master function and called once
+    
+    
+    void EGScr(); //End of game operations to create or update player records - should delete data in plyrsv
+    void DisScre();                             //Displays Scores
+    record *newscre (string,string,int,int);                    //Creates a new Player score record for text file output
+    void updscre (string ,string ,int ,int ,int);              //Updates a players score if found during search
+    void txtRd ();                    //Reads text file score details
+    void txtWrt ();                   //Writes text file score details
+    template<class X,class Y>
+    float ratio(X a,Y b)
+    {   float z;
+    float *x = new float;
+    float *y = new float;
+    *x=a;
+    *y=b;
+        z=*x / *y;
+        delete x;
+        delete y;
+        return (z*100);
+    }
+};
+
+
+
+
+#endif /* BTLSHP_H */
+
